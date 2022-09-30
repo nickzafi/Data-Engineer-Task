8e68aeca7c1b4d1273622b3b6eeb547053ee3773
Nikolaos Zafeiridis <nickzafi@gmail.com>
2022-09-30 11:07:04 +0200

    Step 1

---
+++ init_db_schema_from_models.py
@@ -0,0 +1,21 @@
+from db_connection import initiate_engine
+from models_sql import Base
+import logging
+import sys
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+
+def create_schema(connection_str: str):
+    """
+    Create table schema from models_sql.py for given connection str
+    """
+    try:
+        # create engine
+        engine = initiate_engine(connection_str)
+        # create tables defined at models_sql.py
+        Base.metadata.create_all(engine)
+        logger.info("Tables created")
+    except Exception as e:
+        raise Exception("Failed to create schema {}".format(e))
